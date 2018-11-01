92e720deb517834d64b55e39a43ad63a0fd85e50
joulalan <joulalan@gmail.com>
2018-11-01 12:55:35 +0300

    Initial commit

---
+++ src/main/java/com/tiktak/energymanager/EnergymanagerApplication.java
@@ -0,0 +1,12 @@
+package com.tiktak.energymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergymanagerApplication.class, args);
+    }
+}
